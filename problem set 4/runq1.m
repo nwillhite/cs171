588d360596c02e0d9aa6b731a291086cfb6f3ca2
nwillhite <nwill016@ucr.edu>
2017-05-23 00:29:21 -0700

    added in spliting of training and pruning set

--- problem set 4/runq1.m
+++ problem set 4/runq1.m
@@ -17,15 +17,29 @@ Mtrain = load('banktrain.data');
 Mtest = load('banktestX.data');
 
 %grabs coloumn size for Mtrain
-msize = size(Mtrain,2) - 1;
+[row, col] = size(Mtrain);
+msize = size(Mtrain,2) - 1
+
+% Determine the last row number of the top (upper) 65% of rows.
+lastRow = int32(floor(0.65 * row));
 
 %splits the x and y for training
-trainX = Mtrain(:,[1:msize]);
-trainY = Mtrain(:,end);
+%Get first 65%
+trainX = Mtrain(1:lastRow,[1:col-1]);
+%[trow, tcol] = size(trainX);
+trainY = Mtrain(1:lastRow,end);
+[trow, tcol] = size(trainY)
+
+
+%use the last 35% as the pruning set
+prunX = Mtrain(lastRow+1:end,[1:col-1]);
+%[prow, pcol] = size(prunX);
+prunY = Mtrain(lastRow+1:end,end);
+%[prow, pcol] = size(prunY)
 
 
 
-
-
+Y = 0;
+dt = 0;
 end
 
