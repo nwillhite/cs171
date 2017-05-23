c8ea39b6690d216488514dcbf11d2f80ff5b8709
nwillhite <nwill016@ucr.edu>
2017-05-23 00:02:19 -0700

    updated runq1.m

--- problem set 4/runq1.m
+++ problem set 4/runq1.m
@@ -8,6 +8,23 @@
 
 function [Y,dt] = runq1()
 
+%For the bank dataset the vector should be 
+%(you may hard-code this into your function)
+ftype= [0 12 4 8 3 3 3 2 0 0 0 0 0 3 0 0 0 0 0];
+
+%load in the data sets
+Mtrain = load('banktrain.data');
+Mtest = load('banktestX.data');
+
+%grabs coloumn size for Mtrain
+msize = size(Mtrain,2) - 1;
+
+%splits the x and y for training
+trainX = Mtrain(:,[1:msize]);
+trainY = Mtrain(:,end);
+
+
+
 
 
 end
