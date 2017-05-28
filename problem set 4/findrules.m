6fb14b59bd55c914077eab7c3474609e459ac1eb
nwillhite <nwill016@ucr.edu>
2017-05-28 10:18:34 -0700

    added commments and cleaned up findrules.m

--- problem set 4/findrules.m
+++ problem set 4/findrules.m
@@ -12,32 +12,41 @@ buildingSet = items(D)';
 ruleSet = {};
 numExam = numexamples(D);
 %num = getcount(I(8),D);
-%C = {};
+C = {};
 
 while 1
-    buildingSet = [];
     temp = buildingSet;
+    buildingSet = [];
     [trow,~] = size(temp);
-
+    
+    %gathers the support for each and if greater than smin adds to 
+    %building set
     for i = 1:trow
       sup = support(temp(i,:), D, numExam, smin);
       if sup == 1
         buildingSet = [buildingSet; temp(i,:)];
       end
     end
-
+    
+    %grabs size of the building set
     [brow,~] = size(buildingSet);
-
+    
+    %sends the size of buildingSet row and building set to apri_gen
     set = apriori_gen(brow,buildingSet);
-
+    
+    %sets buildingSet to the set generated from apri_gen
     buildingSet = set;
 
+    %adds buildingSet to ruleSet{}
     ruleSet = [ruleSet buildingSet];
     
+    %checks to see if building set is empty to break out of while 1 loop
     if isempty(buildingSet)
         break
     end
 
 end
+
+   
 
 end %findrules end
